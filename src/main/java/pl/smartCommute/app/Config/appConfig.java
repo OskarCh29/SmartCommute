7885a28923f775f5fd9284075da84b61ed412a97
OskarCh29 <oskarcharytoniuk29@gmail.com>
2025-03-04 11:38:21 +0100

    map Init

---
+++ src/main/java/pl/smartCommute/app/Config/appConfig.java
@@ -0,0 +1,18 @@
+package pl.smartCommute.app.Config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class appConfig {
+
+    @Value("${weather.url}")
+    private String weatherApiUrl;
+
+    @Bean
+    public WebClient webClientSetUp() {
+        return WebClient.builder().baseUrl(weatherApiUrl).build();
+    }
+}
